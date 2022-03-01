93b375e1445b9b578ad16f1fad46062d89adab11
Sashankr <sashankr97@gmail.com>
2022-03-01 23:23:32 +0530

    rendering quiz component when gamestate is playing

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React,{useState} from 'react';
 import './App.css';
 import Menu from './components/Menu';
+import Quiz from './components/Quiz';
 import { GameStateContext } from './helpers/Contexts';
 
 function App() {
@@ -14,6 +15,7 @@ function App() {
      <h1>Quiz App</h1>
      <GameStateContext.Provider value={{gameState,setGameState,username,setUserName}}>
      {gameState === 'menu' && <Menu/>}
+     {gameState === 'playing' && <Quiz/>}
      </GameStateContext.Provider>
     </div>
   );

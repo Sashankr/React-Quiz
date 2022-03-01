832670ee303af6152bcdbad2137c4e723dcb70bb
Sashankr <sashankr97@gmail.com>
2022-03-01 23:13:26 +0530

    setting up context provider

--- src/App.js
+++ src/App.js
@@ -1,16 +1,19 @@
 import React,{useState} from 'react';
 import './App.css';
 import Menu from './components/Menu';
+import { GameStateContext } from './helpers/Contexts';
 
 function App() {
 
-  
   const [gameState,setGameState] = useState('menu'); // ['menu','playing','finished']
+
 
   return (
     <div className="App">
      <h1>Quiz App</h1>
+     <GameStateContext.Provider value={{gameState,setGameState}}>
      {gameState === 'menu' && <Menu/>}
+     </GameStateContext.Provider>
     </div>
   );
 }

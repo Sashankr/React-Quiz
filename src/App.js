a601155af212dd5597f3d20275168fa833aac6f1
Sashankr <sashankr97@gmail.com>
2022-03-01 23:06:38 +0530

    game state variable , to maintain different game states

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
+import React,{useState} from 'react';
 import './App.css';
 import Menu from './components/Menu';
 
 function App() {
+
+  
+  const [gameState,setGameState] = useState('menu'); // ['menu','playing','finished']
+
   return (
     <div className="App">
      <h1>Quiz App</h1>
-     <Menu/>
+     {gameState === 'menu' && <Menu/>}
     </div>
   );
 }

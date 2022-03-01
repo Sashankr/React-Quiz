ea5c65f7843a142599342158fe30c5d0c0c04cb6
Sashankr <sashankr97@gmail.com>
2022-03-01 23:03:34 +0530

    quiz app style changes

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -4,6 +4,7 @@ import '../App.css'
 const Menu = () => {
   return (
     <div className='menu'>
+      <label htmlFor="name">Enter your name : </label>
       <input type="text" placeholder='Example : John Smith'/>
       <button>Start Quiz</button>
     </div>

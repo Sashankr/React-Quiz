c38c2058feb388ef3d2ff83e21a68a4a325eb5aa
Sashankr <sashankr97@gmail.com>
2022-03-01 23:39:12 +0530

    rendering first question and answers

--- src/components/Quiz.js
+++ src/components/Quiz.js
@@ -1,15 +1,27 @@
-import React from 'react'
+import React, { useState } from 'react'
 import '../App.css'
 import { questions } from '../helpers/Questions'
 
 const Quiz = () => {
 
+    const [currentQuestion, setCurrentQuestion] = useState(0);
+    const [optionChosen,setOptionChosen] = useState('');
 
-  return (
-    <div className='quiz'> 
-    quiz
-    </div>
-  )
+    const chooseOption = (option) =>{
+
+    }
+
+    return (
+        <div className='quiz'>
+            <h1>{questions[currentQuestion].propmt}</h1>
+            <div className="answers">
+                <button onClick={()=>{chooseOption('optionA')}}>{questions[currentQuestion].optionA}</button>
+                <button onClick={()=>{chooseOption('optionB')}}>{questions[currentQuestion].optionB}</button>
+                <button onClick={()=>{chooseOption('optionC')}}>{questions[currentQuestion].optionC}</button>
+                <button onClick={()=>{chooseOption('optionD')}}>{questions[currentQuestion].optionD}</button>
+            </div>
+        </div>
+    )
 }
 
 export default Quiz

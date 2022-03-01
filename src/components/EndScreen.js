e6826004916f3cab5e59d8fcc494de22340185e0
Sashankr <sashankr97@gmail.com>
2022-03-02 00:10:01 +0530

    showing username in end screen

--- src/components/EndScreen.js
+++ src/components/EndScreen.js
@@ -5,16 +5,18 @@ import { questions } from '../helpers/Questions';
 
 const EndScreen = () => {
 
-  const {score,setScore,setGameState} = useContext(GameStateContext);
+  const {score , username , setUserName ,setScore,setGameState} = useContext(GameStateContext);
 
   const restartQuiz = () => {
       setScore(0)
       setGameState('menu');
+      setUserName('')
   }
 
   return (
     <div className='endscreen'>
         <h1>Quiz Finished</h1>
+        <h3>Username : {username}</h3>
         <h2>Score : {score} / {questions.length}</h2>
         <button onClick={restartQuiz}>Restart Quiz</button>
     </div>

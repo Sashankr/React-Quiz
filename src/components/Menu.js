8bfc9cfdb13d3b490049b16729ccc18c6b2bf804
Sashankr <sashankr97@gmail.com>
2022-03-01 23:16:39 +0530

    useContext to set global state value from child component

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -1,12 +1,17 @@
 import React from 'react'
 import '../App.css'
+import { useContext } from 'react'
+import { GameStateContext } from '../helpers/Contexts'
 
 const Menu = () => {
+
+  const {gameState,setGameState} = useContext(GameStateContext);
+
   return (
     <div className='menu'>
       <label htmlFor="name">Enter your name : </label>
       <input type="text" placeholder='Example : John Smith'/>
-      <button>Start Quiz</button>
+      <button onClick={()=>{setGameState('playing')}}>Start Quiz</button>
     </div>
   )
 }
